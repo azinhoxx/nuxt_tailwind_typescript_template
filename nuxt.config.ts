6003635a18807543962cd77e7d4d25bb6ed1df2c
azinho <dima.statnyy@gmail.com>
2025-02-28 12:02:26 +0300

    feat: add app.vue, option for process css variables, gitignore archives

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,17 @@
-import { defineNuxtConfig } from 'nuxt/config'
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
   modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/image', '@nuxt/icon'],
-  // @ts-expect-error eslint-configuration
   eslint: {
     config: {
       standalone: false,
     },
+  },
+  fonts: {
+    experimental: {
+      processCSSVariables: true
+    }
   },
   postcss: {
     plugins: {
